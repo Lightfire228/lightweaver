0830c3a1c1276e96254015a420b31bcfbeea35d8
Lightfire228 <lightfire228@gmail.com>
2025-05-15 20:35:55 -0400

    move files

--- src/script/ast/mod.rs
+++ src/script/ast/mod.rs
@@ -7,9 +7,18 @@ mod stmt;
 pub use expr::*;
 pub use stmt::*;
 
+use super::parser::Parser;
+
+pub trait ParseStmt {
+    fn parse(parser: Parser) -> Stmt;
+}
+
+pub trait ParseExpr {
+    fn parse(parser: Parser) -> Expr;
+}
+
 
 #[derive(Debug)]
 pub struct Ast {
     pub stmts: Vec<Stmt>
 }
-
